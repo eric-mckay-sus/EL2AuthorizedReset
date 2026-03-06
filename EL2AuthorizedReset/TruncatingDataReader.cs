0c8c7aa86ea93bdae230510670828858838ae57e
eric-mckay-sus <sus9584@stanley-electric.com>
2026-03-06 07:26:18 -0500

    CSV upload integrated to admin interface (styling could use some work)

--- EL2AuthorizedReset/TruncatingDataReader.cs
+++ EL2AuthorizedReset/TruncatingDataReader.cs
@@ -1,5 +1,6 @@
 using System.Data;
 
+namespace EL2AuthorizedReset;
 /// <summary>
 /// IDataReader wrapper that trims/shortens string values on the fly.
 /// Any column whose name is specified in the constructor will be truncated
