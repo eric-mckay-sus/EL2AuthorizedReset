9c022fa97c76e16f937275c02b7c7dc7962321fc
eric-mckay-sus <sus9584@stanley-electric.com>
2026-03-11 15:29:16 -0400

    Environment variable work

---
+++ loadDbEnv.bat
@@ -0,0 +1,5 @@
+REM Loads the environment variables (as user, not system variables) necessary for using the program
+setx DB_SERVER "SUS-SQL-02"
+setx DB_NAME "EL2AuthorizedReset"
+setx DB_USER "pe3coop"
+setx DB_PASS "pe3coop"
