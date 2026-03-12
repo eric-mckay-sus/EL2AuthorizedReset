084713fb861ea82fffd877764c72000dcca3a8ca
eric-mckay-sus <sus9584@stanley-electric.com>
2026-03-12 09:42:16 -0400

    Reduced DB hits, simple QOL for expand button

--- AdminInterface/UserIdentityService.cs
+++ AdminInterface/UserIdentityService.cs
@@ -1,5 +1,7 @@
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace AdminInterface;
 
@@ -8,7 +10,7 @@ public interface IUserIdentityService
     Task<ClaimsPrincipal> GetUserPrincipalAsync();
 }
 
-public class UserIdentityService(IDbContextFactory<AuthResetDbContext> dbFactory) : IUserIdentityService
+public class UserIdentityService(IDbContextFactory<AuthResetDbContext> dbFactory, IMemoryCache cache) : IUserIdentityService
 {
     // This is fragile, but more secure than simply getting the last 4-5 characters and parsing as an int
     private static readonly string[] stanleyPrefixes = ["SUSU", "SUSD"]; // The prefixes to strip from the username to get the associate number. Put shorter substrings (e.g. SUS for these two) near the end if needed.
@@ -24,11 +26,19 @@ public class UserIdentityService(IDbContextFactory<AuthResetDbContext> dbFactory
     {
         // Get the username from the system (e.g. SUSU1057, SUSD5938)
         string associateString = Environment.UserName;
+        string cacheKey = $"UserPrincipal_{associateString}";
+
+        // If there's an identity stored in the cache, use that
+        if (cache.TryGetValue(cacheKey, out ClaimsPrincipal? cachedPrincipal) && cachedPrincipal != null)
+        {
+            return cachedPrincipal;
+        }
 
         // Check each prefix, attempting a replace on each one (hence the "shorter substring" restriction above)
         foreach (string prefix in stanleyPrefixes) 
             associateString = associateString.Replace(prefix, "");
 
+        ClaimsPrincipal principal;
         // Extract associate number from remaining (hopefully all numeric) characters
         if (int.TryParse(associateString, out int assocNum))
         {
@@ -50,12 +60,19 @@ public class UserIdentityService(IDbContextFactory<AuthResetDbContext> dbFactory
                     claims.Add(new Claim(ClaimTypes.Role, "Admin"));
                 }
 
-                var identity = new ClaimsIdentity(claims, "AutoAuth");
-                return new ClaimsPrincipal(identity);
+                ClaimsIdentity identity = new(claims, "AutoAuth");
+                principal = new(identity);
+            } else
+            {
+                principal = new(new ClaimsIdentity());
             }
-        }
-
         // If int.TryParse failed (prefix strip didn't get something that looked like associate number) or an associate with the parsed number wasn't found in the DB, return anonymous
-        return new ClaimsPrincipal(new ClaimsIdentity());
+        } else
+        {
+            principal = new(new ClaimsIdentity());
+        }
+        cache.Set(cacheKey, principal, TimeSpan.FromMinutes(15));
+
+        return principal;
     }
 }
