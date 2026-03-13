638e8b4d6a58611f5e8983abad40cfae785ab341
eric-mckay-sus <sus9584@stanley-electric.com>
2026-03-13 08:30:49 -0400

    Final touch-ups for historical table filters (staleness updates on input). Added loading state for all pages.

--- AdminInterface/Components/Pages/HomeBase.cs
+++ AdminInterface/Components/Pages/HomeBase.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.EntityFrameworkCore;
 using System.Linq.Dynamic.Core;
 
 namespace AdminInterface.Components.Pages;
@@ -13,6 +11,8 @@ public class HomeBase : EntityManagerBase<Reset, Reset>
     // Filter registry to hold all active filters
     protected Dictionary<string, IFilter> Filters { get; set; } = [];
     private bool _filtersInitialized = false; // Whether the filters have been initialized yet
+    int LastQueryHash;
+    protected bool IsStale => LastQueryHash != GetFilterStateHash(Filters);
 
     // Filter properties that expose the filters in the registry with type safety
     protected Filter<int?> FilterAssocNum => GetFilter<int?>("assocNum");
@@ -24,7 +24,46 @@ public class HomeBase : EntityManagerBase<Reset, Reset>
     protected Filter<DateTime?> FilterEndDate => GetFilter<DateTime?>("before");
 
     /// <summary>
-    /// When the page loads, initialize filters before loading data
+    /// Generate a state ID, for checking equality between two filter states
+    /// 17 and 31 are primes one off of powers of two, so we get few collisions and the compiler can take shortcuts
+    /// </summary>
+    /// <param name="filterDict">A dictionary of keys mapped to filters</param>
+    /// <returns>A value representing the state of the filters for the input dictionary</returns>
+    public int GetFilterStateHash(Dictionary<string, IFilter> filterDict) {
+        unchecked // Tells the compiler to simply truncate the calculation instead of throwing an exception for integer overflow
+        {
+            int hash = 17;
+            // Order by key to ensure dictionary order doesn't change the hash
+            foreach (var key in filterDict.Keys.OrderBy(k => k))
+            {
+                // Ignore 'in' key, it does not affect the search contents within a table
+                if (key.Equals("in", StringComparison.OrdinalIgnoreCase)) continue;
+
+                // Factor in each aspect of the filter
+                var filter = filterDict[key];
+
+                // Key and activity status are part of hash regardless of activity status
+                hash *= 31 + key.ToLower().GetHashCode();
+                hash *= 31 + filter.IsActive.GetHashCode();
+
+                // Only hash filter details if active
+                if (filter.IsActive)
+                {
+                    hash *= 31 + filter.IsNegated.GetHashCode();
+                    string value = filter.GetValue()?.ToString()?.Trim().ToLower() ?? "";
+                    hash *= 31 + value.GetHashCode();
+                }
+            }
+            // Sorts affect view, so a hash of the view should include them
+            hash *= 31 + CurrentSortColumn.GetHashCode();
+            hash *= 31 + SortDir.GetHashCode();
+            
+            return hash;
+        }
+    }
+
+    /// <summary>
+    /// When the page loads, initialize filters and sorts before loading data
     /// </summary>
     protected override async Task OnInitializedAsync()
     {
@@ -32,6 +71,17 @@ public class HomeBase : EntityManagerBase<Reset, Reset>
         SortDir = "descending";
         CurrentSortColumn = "Timestamp";
         await base.OnInitializedAsync();
+    }
+
+    /// <summary>
+    /// Update the query hash after loading data
+    /// </summary>
+    /// <returns></returns>
+    protected override async Task LoadData()
+    {
+        await base.LoadData();
+        LastQueryHash = GetFilterStateHash(Filters);
+        StateHasChanged();
     }
 
     /// <summary>
@@ -76,9 +126,6 @@ public class HomeBase : EntityManagerBase<Reset, Reset>
     /// </summary>
     protected override IQueryable<Reset> ApplyFilter(IQueryable<Reset> query)
     {
-        foreach (var filter in Filters.Values){
-            Console.WriteLine(filter);
-        }
         var filterAssocNum = GetFilter<int?>("assocNum");
         var filterAssocName = GetFilter<string?>("assocName");
         var filterCmmsNum = GetFilter<int?>("cmmsNum");
